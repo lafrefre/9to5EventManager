6e493a5f1531c365fd74d794005415f319ac5268
lafrefre <134004859+lafrefre@users.noreply.github.com>
2023-07-22 12:21:56 -0700

    "added my local storage and functions to js"

---
+++ script.js
@@ -0,0 +1,23 @@
+// im going to use the local storage function to be able to save my jsscript to my html 
+function saveEventToLocalStorage() {
+    localStorage.setItem('events',JSON.stringify(events));
+}
+
+// to be able to load my events from localS. i could use session storage but it wont store my data  after my browser is closed so its not as efficent/
+function loadEventsFromLocalStorage() {
+    var savedEvents = JSON.parse(localStorage.getItem('events)'));
+    events = savedEvents ? savedEvents : {};
+    // using the JSON.parse will be able to convert my text into a js obj
+}
+
+// this funvrion will start the appplication 
+function initApp() {
+    loadEventsFromLocalStorage();
+    // initiating the application will set the function into the works hopefully. My saved events will be pulled from my local storage 
+    renderEvents();
+    // i hope that with "render" and my elemnent of events that it will pull info from my already existing html that has specified.
+    updateCurrentDay();
+    setInterval(updateCurrentDay, 6000);
+    // with the setI it willupdate the current day every min
+}
+
